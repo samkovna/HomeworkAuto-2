482a032045b927f71654691fa0d099bee5f19843
A. Stefanovich <stefanovich.anastasiya@yandex.ru>
2023-05-12 00:41:39 +0300

    changed currency in schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -9,20 +9,19 @@ import static org.hamcrest.Matchers.equalTo;
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-              // код теста
-              .then()
-              .statusCode(200)
-              // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
-              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-              .body("[2].currency", equalTo("RUB"))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                // код теста
+                .then()
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
